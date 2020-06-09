641fbe4f537163dbcd201b2ac59c062291669e44
Advanced UX Cell 신명수 <zommestyle@sk.com>
2020-06-09 20:03:18 +0900

    test audio

--- docs/src/App.js
+++ docs/src/App.js
@@ -3,17 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 
-function App() {
+function button_small(props) {
   return (
-    <div id="root"></div>
-
+    <button
+      id={props.name}
+      className={props.class}>
+    </button>
   );
+}
+
+const btnExit = {
+  name: 'btnExit',
+  class: 'button-small',
+  theme: 'light',
+}
+
+function App() {
   const element = (
+    <button_small
+      name={btnExit.name}
     <div>
       <h1>Hello, world!</h1>
       <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
+  return(element);
 }
-
 export default App;
